9c12bdc7f9ef27b41368d52f44406ad30c609121
08-ayush <aayushchauhan008@gmail.com>
2024-10-09 22:34:47 +0530

    final-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
-import styles from './App.module.css'
-import Display from './components/Display'
-import ButtonsContainer from './components/ButtonsContainer'
+import styles from "./App.module.css";
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+import React, { useState } from "react";
 
 function App() {
-
-
+  const [calval, setCalval] = useState("");
+  const onButtonClick = (buttonTexts) => {
+    if (buttonTexts === "=") {
+      const result = eval(calval);
+      setCalval(result);
+    } else if (buttonTexts === "C") {
+      setCalval("");
+    } else {
+      let newDisplay = calval + buttonTexts;
+      setCalval(newDisplay);
+    }
+  };
   return (
     <>
-       <div className={styles.calculator} >
-         <Display />
-         <ButtonsContainer />
-
-       </div>
+      <div className={styles.calculator}>
+        <Display displayValue={calval} />
+        <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import styles from '../components/ButtonsContainer.module.css'
+import React from "react";
+import styles from "../components/ButtonsContainer.module.css";
 
-const ButtonsContainer = () => {
-
-    const buttonNames = ['C', '1', '2', '+', '3' , '4', '-', '5', '6', '*','7', '8', '/', '=', '9', '0',  '.']
+const ButtonsContainer = ({onButtonClick}) => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <div>
-       <div className={styles.buttonsContainer}>
-        {buttonNames.map((button)=>
-            <button className={styles.buttons}>{button}</button>)}
-         </div>
+      <div className={styles.buttonsContainer}>
+        {buttonNames.map((buttonName) => (
+          <button className={styles.buttons} 
+          onClick={() =>onButtonClick(buttonName)}
+          >{buttonName}</button>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ButtonsContainer
+export default ButtonsContainer;
